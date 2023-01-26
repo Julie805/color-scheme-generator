b2dc646b63e767d4de8ddd97560453560e110579
Julie M <seejulielee@gmail.com>
2023-01-26 07:33:32 -0800

    Set up rendering for correct hex codes from API

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const inputScheme = document.getElementById("select-color-types")
 const getColorBtn = document.getElementById("get-color-btn")
 let seedColor = ""
 let seedScheme = ""
+let colorsArray = []
+let hexArray = []
 
 // captures the picked color value and removes the hashtag
 inputColor.addEventListener("input", () => {
@@ -13,14 +15,36 @@ inputColor.addEventListener("input", () => {
 
 // captures the selected color type
 inputScheme.addEventListener("input", ()=> {
-  seedScheme = inputScheme.value
+  seedScheme = inputScheme.value.toLowerCase()
 })
 
 getColorBtn.addEventListener("click", ()=> {
-  fetch (`https://www.thecolorapi.com/id?hex=${seedColor}&mode=${seedScheme}&count`)
+  fetch (`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${seedScheme}`)
     .then(response => response.json())
-    .then(data => console.log(data)) 
+    .then(data =>  {
+      colorsArray = data.colors 
+      getHex()
+      // renderHex()
+    }    
+ )      
 })
+
+function getHex () { 
+  hexArray = colorsArray.map(color => color.hex.value)
+  renderHex()
+}
+
+function renderHex(){
+  hexArray.forEach(function(hex){
+    document.getElementById("hex-container").innerHTML += `
+    <p class="hex-id">${hex}</p>`
+    document.getElementById("color-container").innerHTML += `
+    <div class="color-swatch">Hi</div>`
+    document.getElementById("color-container").style.backgroundColor = `${hex}`
+  })
+}
+
+
 
 
 
