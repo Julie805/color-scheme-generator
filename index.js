0ee12bcea0a4179a3108ed669f10b1332d68a7d4
Julie M <seejulielee@gmail.com>
2023-01-25 16:01:46 -0800

    Added API request

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-// captures the picked color
-const colorInput = document.getElementById("color-picker")
-colorInput.addEventListener("input", () => {
-  const seedColor = colorInput.value 
-  console.log(seedColor)
+
+const inputColor = document.getElementById("color-picker")
+const inputScheme = document.getElementById("select-color-types")
+const getColorBtn = document.getElementById("get-color-btn")
+let seedColor = ""
+let seedScheme = ""
+
+// captures the picked color value and removes the hashtag
+inputColor.addEventListener("input", () => {
+  hexValue = inputColor.value 
+  seedColor = hexValue.slice(1)
+})
+
+// captures the selected color type
+inputScheme.addEventListener("input", ()=> {
+  seedScheme = inputScheme.value
+})
+
+getColorBtn.addEventListener("click", ()=> {
+  fetch (`https://www.thecolorapi.com/id?hex=${seedColor}&mode=${seedScheme}&count`)
+    .then(response => response.json())
+    .then(data => console.log(data)) 
 })
 
 
 
-// fetch ("https://www.thecolorapi.com/")
+
+
+
+
+
+
+
+// returning undefined in console
+// function getLastColor () {
+//   const lastColor = colorArray[1]
+//   console.log(lastColor) 
+// }
+
+// getLastColor()
+
